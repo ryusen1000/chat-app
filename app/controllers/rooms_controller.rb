619e45eb63a41096747d6a6b6a996df59c65e155
USER <81149239+ryusen1000@users.noreply.github.com>
2021-03-30 12:39:18 +0900

    データ保存時のコントローラー修正点

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -8,7 +8,7 @@ class RoomsController < ApplicationController
   end
 
   def create
-    @room = Room.new
+    @room = Room.new(room_params)
     if @room.save
       redirect_to root_path
     else
